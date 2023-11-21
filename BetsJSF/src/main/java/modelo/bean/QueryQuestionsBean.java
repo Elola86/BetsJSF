c7bd3f358a96796110e749372b3ea33a4ac4d3b0
ehu <ehu@U105288>
2023-11-21 12:58:04 +0100

    faces-config

--- BetsJSF/src/main/java/modelo/bean/QueryQuestionsBean.java
+++ BetsJSF/src/main/java/modelo/bean/QueryQuestionsBean.java
@@ -1,5 +1,29 @@
 package modelo.bean;
 
 public class QueryQuestionsBean {
+	private String nombre;
+	private String password;
+
+	public String getNombre() {
+	 return nombre;
+	 }
+	public void setNombre(String nombre) {
+	 this.nombre = nombre;
+	 }
+	public String getPassword() {
+	 return password;
+	 }
+	public void setPassword(String password) {
+	 this.password = password;
+	 }
+
+	public String comprobar() {
+	 if(nombre.equals("pirata")){
+	 return "error";
+	 }
+	 else {
+	 return "ok";
+	 }
+	 } 
 
 }
