bab8468f81c5f41ad043a5192ee959667a03440b
ehu <ehu@U030075>
2023-12-15 11:45:03 +0100

    mostrar fecha escogida

--- BetsJSF/src/main/java/modelo/bean/QueryQuestionsBean.java
+++ BetsJSF/src/main/java/modelo/bean/QueryQuestionsBean.java
@@ -1,29 +1,46 @@
 package modelo.bean;
 
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.SelectEvent;
+
 public class QueryQuestionsBean {
 	private String nombre;
 	private String password;
+	private Date fecha;
 
 	public String getNombre() {
-	 return nombre;
+		return nombre;
 	 }
 	public void setNombre(String nombre) {
-	 this.nombre = nombre;
+		this.nombre = nombre;
 	 }
 	public String getPassword() {
-	 return password;
+		return password;
 	 }
 	public void setPassword(String password) {
-	 this.password = password;
+		this.password = password;
 	 }
 
 	public String comprobar() {
-	 if(nombre.equals("pirata")){
-	 return "error";
-	 }
-	 else {
-	 return "ok";
-	 }
+		if(nombre.equals("pirata")){
+			return "error";
+		}
+		else {
+			return "ok";
+		}
 	 } 
-
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public void onDateSelect(SelectEvent event) {
+		 FacesContext.getCurrentInstance().addMessage(null,
+		 new FacesMessage("Fecha escogida: "+event.getObject()));
+	} 
 }
