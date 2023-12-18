ebd19a565922f218848d692e4d7b1c8e3cf7a2fd
oierb <oierb@DESKTOP-EK1VB6H>
2023-12-18 23:56:47 +0100

    createQuestionjsf

--- BetsJSF/src/main/java/modelo/bean/CreateQuestionsBean.java
+++ BetsJSF/src/main/java/modelo/bean/CreateQuestionsBean.java
@@ -1,5 +1,66 @@
 package modelo.bean;
 
-public class CreateQuestionsBean {
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+
+import org.primefaces.event.SelectEvent;
+
+import domain.Event;
+
+
+public class CreateQuestionsBean {
+	private String fechaSeleccionada;
+	private Event eventoActual;
+	
+	
+	private List<SelectItem> selectEventos;
+	
+	public List<SelectItem> getSelectEventos() {
+		return selectEventos;
+	}
+
+	public void setSelectEventos(List<SelectItem> selectEventos) {
+		this.selectEventos = selectEventos;
+	}
+
+	public CreateQuestionsBean()
+	{
+		
+		setEventoActual(null);
+		
+		Event evento1 = new Event("Evento 1", new Date());
+		Event evento2 = new Event("Evento 2", new Date());
+       
+        selectEventos = new ArrayList<>();
+    
+        SelectItem selectItem = new SelectItem(evento1, evento1.getDescription());
+        SelectItem selectItem2 = new SelectItem(evento2, evento2.getDescription());
+        selectEventos.add(selectItem);
+        selectEventos.add(selectItem2);
+	}
+	
+	public void guardarFecha(SelectEvent event)
+	{
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Fecha escogida: "+event.getObject()));
+	    fechaSeleccionada = event.getObject().toString();
+		System.out.println(fechaSeleccionada);
+	}
+
+
+
+	public Event getEventoActual() {
+		return eventoActual;
+	}
+
+	public void setEventoActual(Event eventoActual) {
+		this.eventoActual = eventoActual;
+	}
+	
+	
 }
